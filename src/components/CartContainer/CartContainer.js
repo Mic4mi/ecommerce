ff80444c0caa4fa1acc6ebcc9db6135fda9d560b
Micaela Acuña <micahl1981@gmail.com>
2022-09-25 15:46:38 -0300

    cart container

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -1,12 +1,44 @@
 import './style.css';
-import React, { useContext } from 'react'
+import { useContext, useState } from 'react';
 import { CartContext } from '../../context/CartContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { EmptyCart } from '../EmptyCart/EmptyCart';
+import { db } from '../../utils/firebase';
+import { collection, addDoc } from 'firebase/firestore'
 
 export const CartContainer = () => {
     const { cartListProducts, removeItem, clearCart, getTotalPrice } = useContext(CartContext);
+    const [orderID, setOrderID] = useState('');
+    const sendOrder = (event) => {
+        event.preventDefault();
+        const order = {
+            buyer: {
+                name: event.target[0].value,
+                phone: event.target[1].value,
+                email: event.target[2].value
+            },
+            items: cartListProducts,
+            date: new Date(),
+            total: getTotalPrice()
+        };
+        console.log("Orden enviada! ", order);
+        // Crear referencia
+        const queryRef = collection(db, "orders");
+        // Store ref
+        addDoc(queryRef, order)
+            // .then(async function (response) {
+            //     debugger;
+            //     await setOrderID(response.id);
+            //     console.log(`Su orden fue creada. Orden nro ${orderID} generada!!`);
+            // })
+            .then(({id})=>{
+                setOrderID(id);
+                console.log(`Su orden fue creada. Orden nro ${orderID} generada!!`);
+            })
+            .catch(error => console.log("Error: ", error))
+    };
+
     return (
         <div className='cart-container'>
             {
@@ -29,6 +61,15 @@ export const CartContainer = () => {
                         </div>
                         <div className='display-total'>
                             <h4>Total compra: ${getTotalPrice()}</h4>
+                            <form onSubmit={sendOrder}>
+                                <label>Nombre:</label>
+                                <input type="text"></input>
+                                <label>Teléfono</label>
+                                <input type="text"></input>
+                                <label>Correo</label>
+                                <input type="text"></input>
+                                <button type="submit">Send</button>
+                            </form>
                             <button className='purchase-end-btn purchase-btn' onClick={clearCart}>Vaciar carrito</button>
                             <button className='purchase-end-btn purchase-btn'>Pagar</button>
                         </div>
